1bb42a4e37c6076b2247a58beb1f656a89a7d557
Recamm <camiloreartesgm@gmail.com>
2025-07-12 00:08:59 -0300

    Algunas fallas de diseno solucionadas y mejora footer y header

--- js/menuDesplegableHeader.js
+++ js/menuDesplegableHeader.js
@@ -1,18 +1,22 @@
 const menuIcon = document.getElementById('menu-icon');
 const sideMenu = document.getElementById('side-menu');
 const closeMenu = document.getElementById('close-menu');
+const background = document.getElementById("background-menu");
 
 menuIcon.addEventListener('click', () => {
+background.classList.add('active');
 sideMenu.classList.add('active');
 });
 
 closeMenu.addEventListener('click', () => {
+background.classList.remove('active');
 sideMenu.classList.remove('active');
 });
 
 // Opcional: cerrar si se toca fuera del menú
 window.addEventListener('click', (e) => {
 if (!sideMenu.contains(e.target) && !menuIcon.contains(e.target)) {
+    background.classList.remove('active');
     sideMenu.classList.remove('active');
 }
 });
